90654073a9c44b2f4e0afb27fe5c8b25f1ac9f42
Joao Pedro <leopoldino26@gmail.com>
2021-02-19 20:43:11 -0300

    feat: authenticate with jwt and insert user with SHA256

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,7 +6,19 @@ const jwt = require('jsonwebtoken');
 
 
 router.post('/', async (req, res)=>{
-    res.status(200).json({auth: true});
+    const result = await User.findAll({
+        where:{
+            userName: req.body.userName,
+            password: sha256("pp_o_mais_brabo@senac2019.c0m.br"+req.body.password+"15!@hklti_-=130O.asqu")
+        }
+    });
+
+    if(!result.length){
+        res.status(401).json({auth: false})
+    }
+
+    const token = jwt.sign({id: result.id}, '@tiARA', {expiresIn: 600});
+    res.status(200).json({auth: true, token: token});
 });
 
 module.exports = router;
